dbbf4b51b12e04e888487a7be45d04fac405b4f2
eric <lee.eric.03@gmail.com>
2021-05-04 20:48:26 -0600

    commit tree 43d185487197271cebdc2746599bb86cf99ecbf6

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,48 @@
+require 'pry'
+
+# method should return first name of that season's winner
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find do |contestant|
+    contestant["status"] == "Winner"
+  end
+  winner["name"].split[0]
+  # winner["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  matching_contestant = data.values.flatten.find do |contestant|
+    contestant["occupation"] == occupation
+  end
+  matching_contestant["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.count do |contestant|
+    contestant["hometown"] == hometown
+  end 
+  # data.values.flatten.reduce(0) do |count, contestant|
+  #   if contestant["hometown"] == hometown
+  #     count + 1
+  #   else
+  #     count
+  #   end
+  # end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  found_contestant = data.values.flatten.find do |contestant| # find returns hash, so set it equal to variable
+    contestant["hometown"] == hometown
+  end
+  found_contestant["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_ages = data[season].reduce(0) do |acc, contestant|
+    acc + contestant["age"].to_f
+  end
+  # total_ages = data["season"].sum do |contestant|
+  #   contestant["age"].to_f
+  # end
+  total_contestants = data[season].length
+  (total_ages/total_contestants).round
 end
